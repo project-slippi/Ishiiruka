project-slippi/Ishiiruka

commit bbccbe938486cb954bc13f0935610abdc5885f45

Implement support for normal maps, as a base code for  more improvements.

Source/Core/VideoBackends/OGL/ProgramShaderCache.cpp
@@ -605,6 +605,10 @@ void ProgramShaderCache::CreateHeader()
 		"#define lerp mix\n"
 		"#define saturate(x) clamp(x, 0.0, 1.0)\n"
 		"#define mul(x, y) (y * x)\n"
+		"#define ddx dFdx\n"
+		"#define ddy(a) (-dFdy(a))\n"
+		"#define rsqrt inversesqrt\n"
+		
 
 
 		, GetGLSLVersionString().c_str()

Source/Core/DolphinWX/VideoConfigDiag.cpp
@@ -138,6 +138,7 @@ static wxString fullAsyncShaderCompilation_desc = _("Make shader compilation pro
 static wxString waitforshadercompilation_desc = _("Wait for shader compilation in the cpu to avoid fifo problems. This option prevents loops in F-Zero, Metroid Prime fifo resets and others.");
 static wxString predictiveFifo_desc = _("Generate a secondary fifo to predict resource usage and improve loading time.");
 static wxString load_hires_textures_desc = _("Load custom textures from User/Load/Textures/<game_id>/\n\nIf unsure, leave this unchecked.");
+static wxString load_hires_material_maps_desc = _("Load custom material maps from User/Load/Textures/<game_id>/\nUsed to Enable Advanced lighting, Requires Pixel Lighting and Hires Textures Enabled\nIf unsure, leave this unchecked.");
 static wxString cache_hires_textures_desc = _("Cache custom textures to system RAM on startup.\nThis can require exponentially more RAM but fixes possible stuttering.\n\nIf unsure, leave this unchecked.");
 static wxString cache_hires_textures_gpu_desc = _("Cache custom textures to GPU RAM after loading.\nThis can require exponentially more RAM but fixes stuttering the second time the texture is required.\n\nIf unsure, leave this unchecked.");
 static wxString dump_efb_desc = _("Dump the contents of EFB copies to User/Dump/Textures/\n\nIf unsure, leave this unchecked.");
@@ -645,8 +646,10 @@ VideoConfigDiag::VideoConfigDiag(wxWindow* parent, const std::string &title, con
 	szr_utility->Add(CreateCheckBox(page_advanced, _("Load Custom Textures"), (load_hires_textures_desc), vconfig.bHiresTextures));
 	cache_hires_textures = CreateCheckBox(page_advanced, _("Prefetch Custom Textures"), cache_hires_textures_desc, vconfig.bCacheHiresTextures);
 	cache_hires_texturesGPU = CreateCheckBox(page_advanced, _("Cache Custom Textures on GPU"), cache_hires_textures_gpu_desc, vconfig.bCacheHiresTexturesGPU);
+	hires_texturemaps = CreateCheckBox(page_advanced, _("Load Custom Material Maps"), load_hires_material_maps_desc, vconfig.bHiresMaterialMaps);
 	szr_utility->Add(cache_hires_textures);
 	szr_utility->Add(cache_hires_texturesGPU);
+	szr_utility->Add(hires_texturemaps);
 	szr_utility->Add(CreateCheckBox(page_advanced, _("Dump EFB Target"), (dump_efb_desc), vconfig.bDumpEFBTarget));
 	szr_utility->Add(CreateCheckBox(page_advanced, _("Free Look"), (free_look_desc), vconfig.bFreeLook));
 #if !defined WIN32 && defined HAVE_LIBAV
@@ -941,7 +944,8 @@ void VideoConfigDiag::OnUpdateUI(wxUpdateUIEvent& ev)
 	// custom textures
 	cache_hires_textures->Enable(vconfig.bHiresTextures);
 	cache_hires_texturesGPU->Enable(vconfig.bHiresTextures);
-	
+	hires_texturemaps->Enable(vconfig.bHiresTextures && vconfig.bEnablePixelLighting);
+	hires_texturemaps->Show(vconfig.backend_info.bSupportsNormalMaps);
 	// Repopulating the post-processing shaders can't be done from an event
 	if (choice_ppshader && choice_ppshader->IsEmpty())
 		PopulatePostProcessingShaders();

Source/Core/DolphinWX/VideoConfigDiag.h
@@ -139,6 +139,7 @@ protected:
 	SettingRadioButton* virtual_xfb;
 	SettingRadioButton* real_xfb;
 
+	SettingCheckBox* hires_texturemaps;
 	SettingCheckBox* cache_hires_textures;
 	SettingCheckBox* cache_hires_texturesGPU;
 	

Source/Core/VideoBackends/DX11/D3DUtil.cpp
@@ -323,8 +323,6 @@ int CD3DFont::Init()
 		return hr;
 	}
 	D3D::SetDebugObjectName(m_pVB.get(), "vertex buffer of a CD3DFont object");
-	ID3D11ShaderResourceView* view = m_pTexture.get();
-	D3D::context->PSSetShaderResources(8, 1, &view);
 	return S_OK;
 }
 
@@ -374,6 +372,8 @@ int CD3DFont::DrawTextScaled(float x, float y, float size, float spacing, u32 dw
 
 	D3D::stateman->SetInputLayout(m_InputLayout.get());
 	D3D::stateman->SetPrimitiveTopology(D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST);	
+	ID3D11ShaderResourceView* view = m_pTexture.get();
+	D3D::context->PSSetShaderResources(8, 1, &view);
 
 	float fStartX = sx;
 	for (char c : Text)

Source/Core/VideoBackends/DX11/TextureCache.cpp
@@ -32,11 +32,22 @@ D3D::BufferPtr efbcopycbuf[MAX_COPY_BUFFERS];
 TextureCache::TCacheEntry::~TCacheEntry()
 {
 	texture->Release();
+	if (nrm_texture)
+	{
+		nrm_texture->Release();
+	}
 }
 
 void TextureCache::TCacheEntry::Bind(u32 stage)
 {
 	D3D::stateman->SetTexture(stage, texture->GetSRV());
+	if (nrm_texture && g_ActiveConfig.HiresMaterialMapsEnabled())
+	{
+		ID3D11ShaderResourceView* views[1] = {
+			nrm_texture->GetSRV()
+		};
+		D3D::context->PSSetShaderResources(8 + stage, 1, views);
+	}
 }
 
 bool TextureCache::TCacheEntry::Save(const std::string& filename, u32 level)
@@ -160,6 +171,20 @@ void TextureCache::TCacheEntry::Load(const u8* src, u32 width, u32 height,
 		width,
 		height,
 		expanded_width,
+		level,
+		usage,
+		DXGI_format,
+		swap_rg,
+		convertrgb565);
+}
+void TextureCache::TCacheEntry::LoadMaterialMap(const u8* src, u32 width, u32 height, u32 level)
+{
+	D3D::ReplaceTexture2D(
+		nrm_texture->GetTex(),
+		src,
+		width,
+		height,
+		width,
 		level,
 		usage,
 		DXGI_format,
@@ -334,8 +359,20 @@ TextureCache::TCacheEntryBase* TextureCache::CreateTexture(const TCacheEntryConf
 	// TODO: better debug names
 	D3D::SetDebugObjectName(entry->texture->GetTex(), "a texture of the TextureCache");
 	D3D::SetDebugObjectName(entry->texture->GetSRV(), "shader resource view of a texture of the TextureCache");
-
 	SAFE_RELEASE(pTexture);
+	if (config.materialmap)
+	{
+		const D3D11_TEXTURE2D_DESC texdesc = CD3D11_TEXTURE2D_DESC(format,
+			config.width, config.height, config.layers, config.levels, D3D11_BIND_SHADER_RESOURCE, usage, cpu_access);
+
+		const HRESULT hr = D3D::device->CreateTexture2D(&texdesc, NULL, &pTexture);
+		CHECK(SUCCEEDED(hr), "Create material texture of the TextureCache");
+		entry->nrm_texture = new D3DTexture2D(pTexture, D3D11_BIND_SHADER_RESOURCE);
+		// TODO: better debug names
+		D3D::SetDebugObjectName(entry->nrm_texture->GetTex(), "a material texture of the TextureCache");
+		D3D::SetDebugObjectName(entry->nrm_texture->GetSRV(), "shader resource view of a material texture of the TextureCache");
+		SAFE_RELEASE(pTexture);
+	}
 	return entry;
 }
 

Source/Core/VideoBackends/DX11/TextureCache.h
@@ -20,6 +20,7 @@ private:
 	struct TCacheEntry : TCacheEntryBase
 	{
 		D3DTexture2D *texture;
+		D3DTexture2D *nrm_texture;
 		DXGI_FORMAT DXGI_format;
 		D3D11_USAGE usage;
 		bool swap_rg;
@@ -27,7 +28,7 @@ private:
 		bool compressed;
 
 		TCacheEntry(const TCacheEntryConfig& config, D3DTexture2D *_tex) : TCacheEntryBase(config),
-			texture(_tex), swap_rg(false), convertrgb565(false), compressed(false)
+			texture(_tex), nrm_texture(nullptr), swap_rg(false), convertrgb565(false), compressed(false)
 		{}
 		~TCacheEntry();
 
@@ -37,21 +38,21 @@ private:
 			const MathUtil::Rectangle<int> &dstrect) override;
 
 		void Load(const u8* src, u32 width, u32 height,
-			u32 expanded_width, u32 level);
+			u32 expanded_width, u32 level) override;
+		void LoadMaterialMap(const u8* src, u32 width, u32 height, u32 level) override;
 		void Load(const u8* src, u32 width, u32 height, u32 expandedWidth,
-			u32 expandedHeight, const s32 texformat, const u32 tlutaddr, const TlutFormat tlutfmt, u32 level);
+			u32 expandedHeight, const s32 texformat, const u32 tlutaddr, const TlutFormat tlutfmt, u32 level) override;
 		void LoadFromTmem(const u8* ar_src, const u8* gb_src, u32 width, u32 height,
-			u32 expanded_width, u32 expanded_Height, u32 level);
+			u32 expanded_width, u32 expanded_Height, u32 level) override;
 
 		void FromRenderTarget(u8* dst, unsigned int dstFormat, u32 dstStride,
 			PEControl::PixelFormat srcFormat, const EFBRectangle& srcRect,
 			bool isIntensity, bool scaleByHalf, u32 cbufid,
 			const float *colmat) override;
-
-		bool PalettizeFromBase(const TCacheEntryBase* base_entry);
-
-		void Bind(u32 stage);
-		bool Save(const std::string& filename, u32 level);
+		bool SupportsMaterialMap() const override { return nrm_texture != nullptr; };
+		bool PalettizeFromBase(const TCacheEntryBase* base_entry) override;
+		void Bind(u32 stage) override;
+		bool Save(const std::string& filename, u32 level) override;
 	};
 	
 	PC_TexFormat GetNativeTextureFormat(const s32 texformat, const TlutFormat tlutfmt, u32 width, u32 height);

Source/Core/VideoBackends/DX11/main.cpp
@@ -115,7 +115,7 @@ void InitBackendInfo()
 	g_Config.backend_info.bSupportsPostProcessing = true;
 	g_Config.backend_info.bSupportsClipControl = false;
 	g_Config.backend_info.bSupportsSSAA = true;
-	IDXGIFactory* factory;
+	g_Config.backend_info.bSupportsNormalMaps = true;	IDXGIFactory* factory;
 	IDXGIAdapter* ad;
 	hr = DX11::PCreateDXGIFactory(__uuidof(IDXGIFactory), (void**)&factory);
 	if (FAILED(hr))

commit 3b9ca9b581732ad740ce1d9ac8699da078d16bf3

fix for some issues introduced in bbccbe938486cb954bc13f0935610abdc5885f45

Source/Core/VideoBackends/OGL/ProgramShaderCache.cpp
@@ -608,7 +608,7 @@ void ProgramShaderCache::CreateHeader()
 		"#define saturate(x) clamp(x, 0.0, 1.0)\n"
 		"#define mul(x, y) (y * x)\n"
 		"#define ddx dFdx\n"
-		"#define ddy(a) (-dFdy(a))\n"
+		"#define ddy dFdy\n"
 		"#define rsqrt inversesqrt\n"
 		
 

Source/Core/VideoCommon/PixelShaderGen.cpp
@@ -285,9 +285,9 @@ float3x3 cotangent_frame( float3 N, float3 p, float2 uv )
 {
     // get edge vectors of the pixel triangle
     float3 dp1 = ddx( p );
-    float3 dp2 = -ddy( p );
+    float3 dp2 = ddy( p );
     float2 duv1 = ddx( uv );
-    float2 duv2 = -ddy( uv );
+    float2 duv2 = ddy( uv );
  
     // solve the linear system
     float3 dp2perp = cross( dp2, N );
@@ -364,8 +364,8 @@ inline void GeneratePixelShader(T& out, DSTALPHA_MODE dstAlphaMode, u32 componen
 		&& !(bpm.zmode.testenable && bpm.genMode.zfreeze);
 	const bool per_pixel_depth = bpm.zmode.testenable
 		&& ((bpm.ztex2.op != ZTEXTURE_DISABLE && bpm.UseLateDepthTest())
-			|| (!g_ActiveConfig.bFastDepthCalc && !forced_early_z)
-			|| bpm.genMode.zfreeze);
+		|| (!g_ActiveConfig.bFastDepthCalc && !forced_early_z)
+		|| bpm.genMode.zfreeze);
 	bool lightingEnabled = xfr.numChan.numColorChans > 0;
 	bool enable_pl = g_ActiveConfig.bEnablePixelLighting
 		&& g_ActiveConfig.backend_info.bSupportsPixelLighting
@@ -670,6 +670,7 @@ inline void GeneratePixelShader(T& out, DSTALPHA_MODE dstAlphaMode, u32 componen
 			"wu4 tex_ta[%i], tex_t = wu4(0,0,0,0), ras_t = wu4(0,0,0,0), konst_t = wu4(0,0,0,0);\n"
 			"wu3 c16 = wu3(1,256,0), c24 = wu3(1,256,256*256);\n"
 			"wu a_bump=0;\n"
+			"wu3 tevcoord=wu3(0,0,0);\n"
 			"wu2 wrappedcoord=wu2(0,0), t_coord=wu2(0,0),ittmpexp=wu2(0,0);\n"
 			"wu4 tin_a = wu4(0,0,0,0), tin_b = wu4(0,0,0,0), tin_c = wu4(0,0,0,0), tin_d = wu4(0,0,0,0);\n\n", numStages);
 
@@ -809,7 +810,6 @@ inline void GeneratePixelShader(T& out, DSTALPHA_MODE dstAlphaMode, u32 componen
 		{
 			out.Write("\tfloat2 mapcoord = float2(0.0,0.0);\n");
 			out.Write("\tfloat normalmapcount = 0.0;\n");
-			
 		}
 		const char* swapColors = "rgba";
 		for (int i = 0; i < 4; i++)
@@ -865,7 +865,7 @@ inline void GeneratePixelShader(T& out, DSTALPHA_MODE dstAlphaMode, u32 componen
 		}
 		// Only col0 and col1 are needed so discard the remaining components
 		uid_data.components = (components >> 11) & 3;
-		GenerateLightingShader<T, Write_Code>(out, uid_data.lighting, components, I_PMATERIALS, I_PLIGHTS, "colors_", "col", xfr, Use_integer_math,forcePhong);
+		GenerateLightingShader<T, Write_Code>(out, uid_data.lighting, components, I_PMATERIALS, I_PLIGHTS, "colors_", "col", xfr, Use_integer_math, forcePhong);
 	}
 	else
 	{
@@ -1076,8 +1076,7 @@ static inline void WriteFetchStageTexture(T& out, int n, const bool LoadMaterial
 	// HACK to handle cases where the tex gen is not enabled
 	if (!bHasTexCoord)
 		texcoord = 0;
-	out.Write("\n{\n");
-	out.Write("wu3 tevcoord=wu3(0,0,0);\n");
+	out.Write("\n{\n");	
 	if (bHasIndStage)
 	{
 		out.Write("// indirect op\n");
@@ -1374,7 +1373,6 @@ static inline void WriteStage(T& out, pixel_shader_uid_data& uid_data, int n, co
 
 		uid_data.stagehash[n].tevorders_texmap = bpm.tevorders[n / 2].getTexMap(n & 1);
 
-		const char *texswap = swapModeTable[bpm.combiners[n].alphaC.tswap];
 		int texmap = bpm.tevorders[n / 2].getTexMap(n & 1);
 		uid_data.SetTevindrefTexmap(i, texmap);
 