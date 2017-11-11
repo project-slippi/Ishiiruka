dd21ec1c7cdc482a746b98887ea1c36c95201ad3
Hannes Mann <hannesmann2000@gmail.com>
2017-11-11 19:49:52 +0100

    Center pad by default and improve error detection

--- Source/Core/Core/NetPlayClient.cpp
+++ Source/Core/Core/NetPlayClient.cpp
@@ -1077,6 +1077,9 @@ bool NetPlayClient::GetNetPads(const int pad_nb, GCPadStatus* pad_status)
 void NetPlayClient::SendNetPad(int pad_nb)
 {
 	GCPadStatus status = {0};
+	status.stickX = status.stickY =
+	status.substickX = status.substickY =
+	/* these are all the same */ GCPadStatus::MAIN_STICK_CENTER_X;
 
 	// this is the old behavior
 	// just a small lag decrease
