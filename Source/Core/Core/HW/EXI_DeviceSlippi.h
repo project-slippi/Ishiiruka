ef162c8ffcd52f6a223fe9473a8c547f436c153d
Jas Laferriere <Fizzi36@gmail.com>
2020-02-04 02:41:40 -0800

    add support for savestate commands

--- Source/Core/Core/HW/EXI_DeviceSlippi.h
+++ Source/Core/Core/HW/EXI_DeviceSlippi.h
@@ -5,20 +5,23 @@
 #pragma once
 
 #include <SlippiGame.h>
-#include <open-vcdiff/src/google/vcdecoder.h>
-#include <open-vcdiff/src/google/vcencoder.h>
 #include <ctime>
 #include <deque>
-#include <mutex>
 #include <future>
+#include <mutex>
+#include <open-vcdiff/src/google/vcdecoder.h>
+#include <open-vcdiff/src/google/vcencoder.h>
 #include <string>
 #include <unordered_map>
 
 #include "Common/CommonTypes.h"
 #include "Common/FileUtil.h"
-#include "Core/NetPlayClient.h"
 #include "Core/HW/EXI_Device.h"
+#include "Core/NetPlayClient.h"
 #include "Core/Slippi/SlippiReplayComm.h"
+
+#define MELEE_HEAP_START 0x00bd5c40
+#define MELEE_HEAP_SIZE 0x5D7960
 
 // Acts
 class CEXISlippi : public IEXIDevice
@@ -49,7 +52,9 @@ class CEXISlippi : public IEXIDevice
 		CMD_IS_STOCK_STEAL = 0x89,
 		CMD_GET_GECKO_CODES = 0x8A,
 		CMD_GET_FRAME_COUNT = 0x90,
-    CMD_ONLINE_INPUTS = 0xB0,
+		CMD_ONLINE_INPUTS = 0xB0,
+		CMD_CAPTURE_SAVESTATE = 0xB1,
+		CMD_LOAD_SAVESTATE = 0xB2,
 	};
 
 	enum
@@ -60,25 +65,27 @@ class CEXISlippi : public IEXIDevice
 		FRAME_RESP_FASTFORWARD = 3,
 	};
 
-  std::unordered_map<u8, u32> payloadSizes = {
-    // The actual size of this command will be sent in one byte
-    // after the command is received. The other receive command IDs
-    // and sizes will be received immediately following
-    {CMD_RECEIVE_COMMANDS, 1},
+	std::unordered_map<u8, u32> payloadSizes = {
+	    // The actual size of this command will be sent in one byte
+	    // after the command is received. The other receive command IDs
+	    // and sizes will be received immediately following
+	    {CMD_RECEIVE_COMMANDS, 1},
 
-    // The following are all commands used to play back a replay and
-    // have fixed sizes
-    {CMD_PREPARE_REPLAY, 0},
-    {CMD_READ_FRAME, 4},
-    {CMD_IS_STOCK_STEAL, 5},
-    {CMD_GET_LOCATION, 6},
-    {CMD_IS_FILE_READY, 0},
-	  {CMD_GET_GECKO_CODES, 0},
-    {CMD_GET_FRAME_COUNT, 0},
+	    // The following are all commands used to play back a replay and
+	    // have fixed sizes
+	    {CMD_PREPARE_REPLAY, 0},
+	    {CMD_READ_FRAME, 4},
+	    {CMD_IS_STOCK_STEAL, 5},
+	    {CMD_GET_LOCATION, 6},
+	    {CMD_IS_FILE_READY, 0},
+	    {CMD_GET_GECKO_CODES, 0},
+	    {CMD_GET_FRAME_COUNT, 0},
 
-    // The following are used for Slippi online and also have fixed sizes
-    {CMD_ONLINE_INPUTS, 17}
-  };
+	    // The following are used for Slippi online and also have fixed sizes
+	    {CMD_ONLINE_INPUTS, 17},
+	    {CMD_CAPTURE_SAVESTATE, 20},
+	    {CMD_LOAD_SAVESTATE, 20},
+	};
 
 	// Communication with Launcher
 	SlippiReplayComm *replayComm;
@@ -106,11 +113,11 @@ class CEXISlippi : public IEXIDevice
 	File::IOFile m_file;
 	std::vector<u8> m_payload;
 
-  // online play stuff
-  void handleOnlineInputs(u8* payload);
-  void prepareOpponentInputs(u8* payload);
-  void handleSendInputs(u8 *payload);
-  bool shouldSkipOnlineFrame(int32_t frame);
+	// online play stuff
+	void handleOnlineInputs(u8 *payload);
+	void prepareOpponentInputs(u8 *payload);
+	void handleSendInputs(u8 *payload);
+	bool shouldSkipOnlineFrame(int32_t frame);
 
 	// replay playback stuff
 	void prepareGameInfo();
@@ -123,14 +130,15 @@ class CEXISlippi : public IEXIDevice
 	void prepareIsFileReady();
 	void processInitialState(std::vector<u8> &iState);
 	void resetPlayback();
-	void clearWatchSettingsStartEnd(); 
+	void clearWatchSettingsStartEnd();
 
 	void SavestateThread(void);
 	void SeekThread(void);
-	
-	std::unordered_map<int32_t, std::shared_future<std::string>> futureDiffs; // State diffs keyed by frameIndex, processed async
-	std::vector<u8> iState;                                            // The initial state
-	std::vector<u8> cState;                                            // The current (latest) state
+
+	std::unordered_map<int32_t, std::shared_future<std::string>>
+	    futureDiffs;        // State diffs keyed by frameIndex, processed async
+	std::vector<u8> iState; // The initial state
+	std::vector<u8> cState; // The current (latest) state
 
 	bool shouldFFWToTarget = false;
 	int mostRecentlyProcessedFrame = INT_MAX;
@@ -140,7 +148,7 @@ class CEXISlippi : public IEXIDevice
 
 	std::unordered_map<u8, std::string> getNetplayNames();
 
-  std::vector<uint8_t> geckoList;
+	std::vector<uint8_t> geckoList;
 
 	bool isSoftFFW = false;
 	bool isHardFFW = false;
@@ -152,5 +160,23 @@ class CEXISlippi : public IEXIDevice
 	void TransferByte(u8 &byte) override;
 
   private:
-  std::unique_ptr<NetPlayClient> slippi_netplay;
+	std::unique_ptr<NetPlayClient> slippi_netplay;
+
+	typedef struct
+	{
+		bool isGame;
+		u32 address;
+		u32 size;
+		u8 *data;
+		u8 **nonGamePtr;
+	} ssBackupLoc;
+
+	std::vector<ssBackupLoc> backupLocs = {
+	    //{true, 0x00bd5c40, 0x5D7960, NULL}, // Heap
+	    //{0x004316c0, 0xA6309, NULL}, // BSS
+	    //{0x004d79e0, 0x7220, NULL}, // Data Section 7?
+	    //{0x004dec00, 0x10000, NULL}, // Stack
+	    //{true, 0x00005520, 0x420, NULL}, // Data Sections 0 and 1
+	    //{true, 0x003b7240, 0x1279C0, NULL}, // Data Sections 2-7 and in between sections
+	};
 };
