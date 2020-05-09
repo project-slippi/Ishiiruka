9e427a7949a33bf516d0acdce22355dedb420ab8
Jas Laferriere <Fizzi36@gmail.com>
2020-05-09 16:06:28 -0700

    add logic for connecting to enet mm service

--- Source/Core/Core/Slippi/SlippiMatchmaking.h
+++ Source/Core/Core/Slippi/SlippiMatchmaking.h
@@ -6,8 +6,12 @@
 #include "Core/Slippi/SlippiUser.h"
 
 #include <curl/curl.h>
+#include <enet/enet.h>
 #include <unordered_map>
 #include <vector>
+
+#include <json.hpp>
+using json = nlohmann::json;
 
 class SlippiMatchmaking
 {
@@ -32,7 +36,13 @@ class SlippiMatchmaking
 	std::unique_ptr<SlippiNetplayClient> GetNetplayClient();
 
   protected:
-	const std::string URL_START = "http://35.197.121.196:43113/tickets";
+	const std::string MM_HOST = "35.197.121.196";
+	const u16 MM_PORT = 43113;
+
+	ENetHost *m_client;
+	ENetPeer *m_server;
+
+	bool isMmConnected = false;
 
 	CURL *m_curl = nullptr;
 	struct curl_slist *m_curlHeaderList = nullptr;
@@ -43,6 +53,7 @@ class SlippiMatchmaking
 
 	SlippiUser *m_user;
 
+  int m_hostPort;
 	std::string m_ticketId;
 	std::string m_oppIp;
 	bool m_isHost;
@@ -58,6 +69,11 @@ class SlippiMatchmaking
 	    {ProcessState::MATCHMAKING, true},
 	    {ProcessState::OPPONENT_CONNECTING, true},
 	};
+
+  void disconnectFromServer();
+  void terminateMmConnection();
+	void sendMessage(json msg);
+	int receiveMessage(json &msg, int maxAttempts);
 
 	void startMatchmaking();
 	void handleMatchmaking();
