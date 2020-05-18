af6ddfc63f3286c8de06e9104bca804fa4605d53
Jas Laferriere <Fizzi36@gmail.com>
2020-05-17 21:08:21 -0700

    fix bugs with slippi timer causing playback crashes

--- Source/Core/Core/SlippiTimer.cpp
+++ Source/Core/Core/SlippiTimer.cpp
@@ -6,6 +6,12 @@ extern std::unique_ptr<SlippiPlaybackStatus> g_playback_status;
 
 void slippiTimer::Notify()
 {
+	if (!m_slider || !m_text)
+	{
+		// If there is no slider, do nothing
+		return;
+	}
+
 	int totalSeconds = (int) ((g_playback_status->latestFrame + 123) / 60);
 	int totalMinutes = (int)(totalSeconds / 60);
 	int totalRemainder = (int)(totalSeconds % 60);
@@ -14,14 +20,14 @@ void slippiTimer::Notify()
 	int currMinutes = (int)(currSeconds / 60);
 	int currRemainder = (int)(currSeconds % 60);
 	// Position string (i.e. MM:SS)
-	char endTime[5];
+	char endTime[32];
 	sprintf(endTime, "%02d:%02d", totalMinutes, totalRemainder);
-	char currTime[5];
+	char currTime[32];
 	sprintf(currTime, "%02d:%02d", currMinutes, currRemainder);
 
 	std::string time = std::string(currTime) + " / " + std::string(endTime);
 
-	// Setup the slider and gauge min/max values
+  // Setup the slider and gauge min/max values
 	int minValue = m_slider->GetMin();
 	int maxValue = m_slider->GetMax();
 	if (maxValue != (int)g_playback_status->latestFrame || minValue != -123)
