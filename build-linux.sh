b9b1c66f0fca578efe204d6e8a0c1b861f2f4f99
meta <hosaka-corp@users.noreply.github.com>
2020-06-27 04:51:13 -0500

    linux: add simple arguments to build script

--- build-linux.sh
+++ build-linux.sh
@@ -1,8 +1,19 @@
 #!/bin/bash -e
 # build-linux.sh
 
-# Arguments passed to CMake
-CMAKE_FLAGS=''
+case "${1}" in
+	# Enables portable configuration files via `portable.txt`
+	"portable")
+		CMAKE_FLAGS='-DLINUX_LOCAL_DEV=true'
+		;;
+	"appimage")
+		CMAKE_FLAGS=''
+		;;
+	*)
+		echo "usage: ${0} <portable|appimage>"
+		exit 0
+		;;
+esac
 
 # Create the build directory if it doesn't exist
 if [ ! -e "./build/" ]; then  mkdir ./build; fi
