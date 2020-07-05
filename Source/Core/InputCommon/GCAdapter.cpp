bd21f948e8075267050ec3fc53ee849939ee6eb0
Jas Laferriere <Fizzi36@gmail.com>
2020-07-05 00:23:34 -0700

    remove previous neutralize code

--- Source/Core/InputCommon/GCAdapter.cpp
+++ Source/Core/InputCommon/GCAdapter.cpp
@@ -85,20 +85,24 @@ static void Read()
 	int payload_size = 0;
 	while (s_adapter_thread_running.IsSet())
 	{
+		bool reuseOldInputsEnabled = SConfig::GetInstance().bAdapterWarning;
 		adapter_error = libusb_interrupt_transfer(s_handle, s_endpoint_in, s_controller_payload_swap,
-			sizeof(s_controller_payload_swap), &payload_size, 16) != LIBUSB_SUCCESS && SConfig::GetInstance().bAdapterWarning;
+			sizeof(s_controller_payload_swap), &payload_size, 16) != LIBUSB_SUCCESS && reuseOldInputsEnabled;
 
 		// Store previous input and restore in the case of an adapter error
-		if (!adapter_error)
+		if (reuseOldInputsEnabled)
 		{
-			memcpy(bkp_payload_swap, s_controller_payload_swap, 37);
-			bkp_payload_size = payload_size;
-			has_prev_input = true;
-		}
-		else if (has_prev_input)
-		{
-			memcpy(s_controller_payload_swap, bkp_payload_swap, 37);
-			payload_size = bkp_payload_size;
+			if (!adapter_error)
+			{
+				memcpy(bkp_payload_swap, s_controller_payload_swap, 37);
+				bkp_payload_size = payload_size;
+				has_prev_input = true;
+			}
+			else if (has_prev_input)
+			{
+				memcpy(s_controller_payload_swap, bkp_payload_swap, 37);
+				payload_size = bkp_payload_size;
+			}
 		}
 
 		{
@@ -424,16 +428,6 @@ GCPadStatus Input(int chan)
 
 	if (s_handle == nullptr || !s_detected)
 		return{};
-
-	if(AdapterError())
-	{
-		GCPadStatus centered_status = {0};
-		centered_status.stickX = centered_status.stickY =
-		centered_status.substickX = centered_status.substickY =
-		/* these are all the same */ GCPadStatus::MAIN_STICK_CENTER_X;
-
-		return centered_status;
-	}
 
 	int payload_size = 0;
 	u8 controller_payload_copy[37];
