b203fcf88224687c6401e3a54806a135f61dd7b1
Nikki Narayana <nikhil.narayana@live.com>
2020-07-12 14:43:17 -0700

    build: add playback arg to build-linux.sh

--- build-linux.sh
+++ build-linux.sh
@@ -3,6 +3,16 @@
 
 CMAKE_FLAGS='-DLINUX_LOCAL_DEV=true'
 
+# Build
+if [ -z "$1" ]
+    then
+        echo "Using Netplay build config"
+elif [ "$1" == "playback" ]
+    then
+        CMAKE_FLAGS+=" -DIS_PLAYBACK=true"
+        echo "Using Playback build config"
+fi
+
 # Move into the build directory, run CMake, and compile the project
 mkdir -p build
 pushd build
