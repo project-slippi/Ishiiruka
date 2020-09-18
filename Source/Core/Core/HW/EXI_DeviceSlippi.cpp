14eabb4704a9f62dd3ab3de9e39fd7e96328b5fa
Robert Peralta <robert.peralta@helloglobo.com>
2020-09-18 01:10:36 -0400

    Always set names in CSS

--- Source/Core/Core/HW/EXI_DeviceSlippi.cpp
+++ Source/Core/Core/HW/EXI_DeviceSlippi.cpp
@@ -1875,6 +1875,10 @@ void CEXISlippi::prepareOnlineMatchState()
         {
             sentChatMessage = StringFromFormat("%s: %s", userInfo.displayName.c_str(), predefinedChatMessages[sentChatMessageId].c_str());
         }
+
+        // in CSS p1 is always current player and p2 is opponent
+        p1Name = userInfo.displayName;
+        p2Name = oppName;
     }
 
 	auto directMode = SlippiMatchmaking::OnlinePlayMode::DIRECT;
