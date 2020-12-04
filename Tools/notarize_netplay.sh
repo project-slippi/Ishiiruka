fe768118b370c2dfb124b334e56bae7b22c5e792
Ryan McGrath <ryan@rymc.io>
2020-12-04 12:54:16 -0800

    Change the notarize script to explicitly loop for up to 10 minutes, bailing if it encounters either error or success. Logs statuses slightly more to help with debugging as well.

--- Tools/notarize_netplay.sh
+++ Tools/notarize_netplay.sh
@@ -16,6 +16,14 @@ requeststatus() { # $1: requestUUID
     echo "$req_status"
 }
 
+logstatus() { # $1: requestUUID
+    requestUUID=${1?:"need a request UUID"}
+    xcrun altool --notarization-info "$requestUUID" \
+        --apiKey "${APPLE_API_KEY}" \
+        --apiIssuer "${APPLE_ISSUER_ID}"
+    echo
+}
+
 notarizefile() { # $1: path to file to notarize, $2: identifier
     filepath=${1:?"need a filepath"}
     identifier=${2:?"need an identifier"}
@@ -37,28 +45,32 @@ notarizefile() { # $1: path to file to notarize, $2: identifier
     fi
         
     # wait for status to be not "in progress" any more
-    request_status="in progress"
-    while [[ "$request_status" == "in progress" ]]; do
-        echo -n "waiting... "
-        sleep 10
+    # Checks for up to ~10 minutes ((20 * 30s = 600) / 60s)
+    for i ({0..20}); do
         request_status=$(requeststatus "$requestUUID")
-        echo "$request_status"
+        echo "Status: ${request_status}"
+
+        # Why can this report two different cases...?
+        if [ $? -ne 0 ] || [[ "${request_status}" =~ "invalid" ]] || [[ "${request_status}" =~ "Invalid" ]]; then
+            logstatus "$requestUUID"
+            echo "Error with notarization. Exiting!"
+            exit 1
+        fi
+
+        if [[ "${request_status}" =~ "success" ]]; then
+            logstatus "$requestUUID"
+            echo "Successfully notarized! Stapling notarization status to ${filepath}"
+            xcrun stapler staple "$filepath"
+            exit 0
+        fi
+
+        echo "Still in progress, will check again in 30s"
+        sleep 30
     done
-    
-    # print status information
-    xcrun altool --notarization-info "$requestUUID" \
-        --apiKey "${APPLE_API_KEY}" \
-        --apiIssuer "${APPLE_ISSUER_ID}"
-    echo 
-    
-    if [[ $request_status != "success" ]]; then
-        echo "## could not notarize $filepath"
-        exit 1
-    fi 
+
+    echo "Notarization request timed out - status below; maybe it needs more time?"
+    logstatus "$requestUUID"
 }
 
-echo "Attempt notarization"
+echo "Attempting notarization"
 notarizefile "$1" "$identifier"
-
-echo "Staple notarization status"
-xcrun stapler staple "$1"
