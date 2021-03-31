6cd9c0760b011262bdadeb2f87c49e37891274fb
Robert Peralta <rapito@gmail.com>
2021-03-31 18:01:39 -0400

    format

--- Source/Core/Core/HW/EXI_DeviceSlippi.cpp
+++ Source/Core/Core/HW/EXI_DeviceSlippi.cpp
@@ -2470,7 +2470,7 @@ std::vector<u8> CEXISlippi::loadPremadeText(u8 *payload)
 
 		//DEBUG_LOG(SLIPPI, "SLIPPI premade text param: 0x%x", payload[1]);
 		u8 paramId = payload[1] == 0x83 ? 0x88 : payload[1]; // TODO: Figure out what the hell is going on and fix this
-        playerName = ReplaceAll(playerName.c_str(), " ", "<S>");
+		playerName = ReplaceAll(playerName.c_str(), " ", "<S>");
 
 		if (paramId == SlippiPremadeText::CHAT_MSG_CHAT_DISABLED)
 		{
