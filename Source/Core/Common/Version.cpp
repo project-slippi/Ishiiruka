4ed4d68706ab862641a9797b0ddee2e40da455a6
Julien Bernard <artessbm@gmail.com>
2021-04-20 00:20:29 +0200

    Suppress update

--- Source/Core/Common/Version.cpp
+++ Source/Core/Common/Version.cpp
@@ -23,7 +23,8 @@
 //#else
 //" " BUILD_TYPE_STR " " SCM_DESC_STR;
 //#endif
-#define SLIPPI_REV_STR "2.3.0-beta.4"
+#define SLIPPI_REV_STR "3.0.0"
+//"2.3.0-beta.4"
 #ifdef IS_PLAYBACK
 const std::string scm_rev_str = "Faster Melee - Slippi (" SLIPPI_REV_STR ") - Playback";
 #else
