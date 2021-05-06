27ec6eaea6bf8f1d4faa546c1a27de93436206e6
Jas Laferriere <Fizzi36@gmail.com>
2021-05-06 04:41:17 -0700

    add mx and scene files

--- Source/Core/Core/HW/EXI_DeviceSlippi.cpp
+++ Source/Core/Core/HW/EXI_DeviceSlippi.cpp
@@ -41,7 +41,7 @@
 #define SLEEP_TIME_MS 8
 #define WRITE_FILE_SLEEP_TIME_MS 85
 
-//#define LOCAL_TESTING
+#define LOCAL_TESTING
 //#define CREATE_DIFF_FILES
 
 static std::unordered_map<u8, std::string> slippi_names;
