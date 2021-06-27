4ef3e2a778e6ccd61a757a078e20f8ddbdae6487
Nikhil Narayana <nikhil.narayana@live.com>
2021-06-26 17:30:42 -0700

    feat: don't default Render to Main Window true on Linux

--- Source/Core/Core/ConfigManager.cpp
+++ Source/Core/Core/ConfigManager.cpp
@@ -531,7 +531,7 @@ void SConfig::LoadDisplaySettings(IniFile &ini)
 
 	display->Get("Fullscreen", &bFullscreen, false);
 	display->Get("FullscreenResolution", &strFullscreenResolution, "Auto");
-#ifdef IS_PLAYBACK
+#ifdef defined IS_PLAYBACK && (defined _WIN32 || defined __APPLE__)
 	display->Get("RenderToMain", &bRenderToMain, true);
 #else
 	display->Get("RenderToMain", &bRenderToMain, false);
