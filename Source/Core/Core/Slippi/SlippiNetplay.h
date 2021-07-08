5548ff4f6268ce19a4137b877b1d0ebdd43fedd6
AltF4 <altf4petro@gmail.com>
2021-07-07 23:42:59 -0700

    Validate incoming chat and player selection packets (#290)
    * Validate incoming chat and player selection packets
     - Reject all but the expected 16 chat IDs
     - Validate that each parameter of each packet exists

--- Source/Core/Core/Slippi/SlippiNetplay.h
+++ Source/Core/Core/Slippi/SlippiNetplay.h
@@ -53,7 +53,8 @@ class SlippiPlayerSelections
 
 	u32 rngOffset = 0;
 
-	int messageId;
+	int messageId = 0;
+	bool error = false;
 
 	void Merge(SlippiPlayerSelections &s)
 	{
@@ -192,7 +193,7 @@ class SlippiNetplayClient
 	bool hasGameStarted = false;
 	u8 playerIdx = 0;
 
-	std::unordered_map<std::string, std::map<ENetPeer*, bool>> activeConnections;
+	std::unordered_map<std::string, std::map<ENetPeer *, bool>> activeConnections;
 
 	std::deque<std::unique_ptr<SlippiPad>> localPadQueue; // most recent inputs at start of deque
 	std::deque<std::unique_ptr<SlippiPad>>
