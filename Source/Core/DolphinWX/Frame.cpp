7725d5fb0cae6911d093a444a59a238649784ec8
Natalie Dorshimer <dorshimer.natalie@gmail.com>
2021-08-24 17:26:53 -0400

    make naming more explicit

--- Source/Core/DolphinWX/Frame.cpp
+++ Source/Core/DolphinWX/Frame.cpp
@@ -1245,7 +1245,7 @@ void CFrame::PollHotkeys(wxTimerEvent &event)
 	}
 }
 
-static void RefreshInputDevices() {
+static void RefreshInputConfig() {
 	bool was_unpaused = Core::PauseAndLock(true);
 
 	Wiimote::LoadConfig();
@@ -1334,8 +1334,8 @@ void CFrame::ParseHotkeys()
 		Core::SaveScreenShot();
 	if (IsHotkey(HK_EXIT))
 		wxPostEvent(this, wxCommandEvent(wxEVT_MENU, wxID_EXIT));
-	if (IsHotkey(HK_REFRESH_PAD))
-		RefreshInputDevices();
+	if (IsHotkey(HK_REFRESH_INPUT_CONFIG))
+		RefreshInputConfig();
 	if (IsHotkey(HK_VOLUME_DOWN))
 		AudioCommon::DecreaseVolume(3);
 	if (IsHotkey(HK_VOLUME_UP))
