b71ebd5678409f0e5a05ffb5f341db5c2ce56166
Jas Laferriere <Fizzi36@gmail.com>
2022-08-17 18:32:18 -0400

    fix: support generic convert function
    changes SlippiExiTypes to a namespace, hopefully this fixes the build issues too. I hate C++

--- Source/Core/Core/Slippi/SlippiExiTypes.h
+++ Source/Core/Core/Slippi/SlippiExiTypes.h
@@ -4,75 +4,100 @@
 
 #define REPORT_PLAYER_COUNT 4
 
-class SlippiExiTypes
+namespace SlippiExiTypes
 {
-  public:
 // Using pragma pack here will remove any structure padding which is what EXI comms expect
 // https://www.geeksforgeeks.org/how-to-avoid-structure-padding-in-c/
 // Note that none of these classes should be used outside of the handler function, pragma pack
 // is supposedly not very efficient?
 #pragma pack(1)
 
-	struct ReportGameQueryPlayer
-	{
-		u8 slotType;
-		u8 stocksRemaining;
-		float damageDone;
-	};
+struct ReportGameQueryPlayer
+{
+	u8 slotType;
+	u8 stocksRemaining;
+	float damageDone;
+};
 
-	struct ReportGameQuery
-	{
-		u8 command;
-		u8 onlineMode;
-		u32 frameLength;
-		u32 gameIndex;
-		u32 tiebreakIndex;
-		s8 winnerIdx;
-		u8 gameEndMethod;
-		s8 lrasInitiator;
-		ReportGameQueryPlayer players[REPORT_PLAYER_COUNT];
-		u8 gameInfoBlock[312];
-	};
+struct ReportGameQuery
+{
+	u8 command;
+	u8 onlineMode;
+	u32 frameLength;
+	u32 gameIndex;
+	u32 tiebreakIndex;
+	s8 winnerIdx;
+	u8 gameEndMethod;
+	s8 lrasInitiator;
+	ReportGameQueryPlayer players[REPORT_PLAYER_COUNT];
+	u8 gameInfoBlock[312];
+};
 
-	struct GpCompleteStepQuery
-	{
-		u8 command;
-		u8 step_idx;
-		u8 char_selection;
-		u8 char_color_selection;
-		u8 stage_selections[2];
-	};
+struct GpCompleteStepQuery
+{
+	u8 command;
+	u8 step_idx;
+	u8 char_selection;
+	u8 char_color_selection;
+	u8 stage_selections[2];
+};
 
-	struct GpFetchStepQuery
-	{
-		u8 command;
-		u8 step_idx;
-	};
+struct GpFetchStepQuery
+{
+	u8 command;
+	u8 step_idx;
+};
 
-	struct GpFetchStepResponse
-	{
-		u8 is_found;
-		u8 is_skip;
-		u8 char_selection;
-		u8 char_color_selection;
-		u8 stage_selections[2];
-	};
+struct GpFetchStepResponse
+{
+	u8 is_found;
+	u8 is_skip;
+	u8 char_selection;
+	u8 char_color_selection;
+	u8 stage_selections[2];
+};
 
-	struct OverwriteCharSelections
-	{
-		u8 is_set;
-		u8 char_id;
-		u8 char_color_id;
-	};
-	struct OverwriteSelectionsQuery
-	{
-		u8 command;
-		u16 stage_id;
-		OverwriteCharSelections chars[4];
-	};
+struct OverwriteCharSelections
+{
+	u8 is_set;
+	u8 char_id;
+	u8 char_color_id;
+};
+struct OverwriteSelectionsQuery
+{
+	u8 command;
+	u16 stage_id;
+	OverwriteCharSelections chars[4];
+};
 
 // Not sure if resetting is strictly needed, might be contained to the file
 #pragma pack()
 
-	template <typename T> static T Convert(u8 *payload);
-};
+template <typename T> static T Convert(u8 *payload)
+{
+	return *reinterpret_cast<T *>(payload);
+}
+
+// Here we define custom convert functions for any type that larger than u8 sized fields to convert from big-endian
+
+template <> static ReportGameQuery Convert(u8 *payload)
+{
+	auto q = *reinterpret_cast<ReportGameQuery *>(payload);
+	q.frameLength = Common::FromBigEndian(q.frameLength);
+	q.gameIndex = Common::FromBigEndian(q.gameIndex);
+	q.tiebreakIndex = Common::FromBigEndian(q.tiebreakIndex);
+	for (int i = 0; i < REPORT_PLAYER_COUNT; i++)
+	{
+		auto *p = &q.players[i];
+		p->damageDone = Common::FromBigEndian(p->damageDone);
+	}
+	return q;
+}
+
+template <> static OverwriteSelectionsQuery Convert(u8 *payload)
+{
+	auto q = *reinterpret_cast<OverwriteSelectionsQuery *>(payload);
+	q.stage_id = Common::FromBigEndian(q.stage_id);
+	return q;
+}
+}; // namespace SlippiExiTypes
