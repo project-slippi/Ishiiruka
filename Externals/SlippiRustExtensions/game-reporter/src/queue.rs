fd8e8fb372735442c0b1764b2883f5bb01731d9e
Jas Laferriere <Fizzi36@gmail.com>
2023-08-05 14:13:00 -0400

    add file uploading logic
    still doesn't quite work, hence why I left a bunch of loggers in there.
    
    compression seems to work but uploading gets a 400 bad request response. I tried both post and put, both failed

--- Externals/SlippiRustExtensions/game-reporter/src/queue.rs
+++ Externals/SlippiRustExtensions/game-reporter/src/queue.rs
@@ -11,6 +11,10 @@ use dolphin_logger::Log;
 use crate::types::{GameReport, GameReportRequestPayload};
 use crate::ProcessingEvent;
 
+use flate2::write::GzEncoder;
+use flate2::Compression;
+use std::io::Write;
+
 const GRAPHQL_URL: &str = "https://gql-gateway-dev-dot-slippi.uc.r.appspot.com/graphql";
 
 /// How many times a report should attempt to send.
@@ -20,6 +24,8 @@ const MAX_REPORT_ATTEMPTS: i32 = 5;
 #[derive(Debug, serde::Deserialize)]
 struct ReportResponse {
     success: bool,
+
+    #[serde(rename = "uploadUrl")]
     upload_url: Option<String>,
 }
 
@@ -199,8 +205,15 @@ fn process_reports(queue: &GameReporterQueue, event: ProcessingEvent) {
                 // to the replay uploader.
                 let report = report_queue.pop_front();
 
+                tracing::warn!(
+                    target: Log::GameReporter,
+                    url = ?upload_url,
+                    len = report.as_ref().map(|r| r.replay_data.len()),
+                    "Successfully sent report, popping from queue",
+                );
+
                 if let (Some(report), Some(upload_url)) = (report, upload_url) {
-                    try_upload_replay_data(report.replay_data, upload_url);
+                    try_upload_replay_data(report.replay_data, upload_url, &queue.http_client);
                 }
 
                 thread::sleep(Duration::ZERO)
@@ -343,7 +356,8 @@ fn try_send_next_report(
             }
         })?;
 
-    // tracing::error!(target: Log::GameReporter, "Response {}", response_body);
+    tracing::error!(target: Log::GameReporter, "Response {}", response_body);
+    tracing::error!(target: Log::GameReporter, "1????");
 
     // Now, parse the response JSON to get the data you need.
     let response: ReportResponse = serde_json::from_str(&response_body).map_err(|e| ReportSendError {
@@ -351,6 +365,8 @@ fn try_send_next_report(
         sleep_ms: error_sleep_ms,
         kind: ReportSendErrorKind::JSON(e),
     })?;
+
+    tracing::error!(target: Log::GameReporter, "2????");
 
     if !response.success {
         return Err(ReportSendError {
@@ -360,10 +376,96 @@ fn try_send_next_report(
         });
     }
 
+    tracing::error!(target: Log::GameReporter, "????");
+    tracing::error!(target: Log::GameReporter, url = ?response.upload_url, "Upload URL");
+
     Ok(response.upload_url)
 }
 
+// Add the "flate2" crate to your Cargo.toml to use the flate2 library for compression and decompression.
+
+// Ported compressToGzip function
+fn compress_to_gzip(input: &[u8], output: &mut [u8]) -> usize {
+    let mut encoder = GzEncoder::new(output, Compression::default());
+    let write_res = encoder.write_all(input);
+    if let Err(e) = write_res {
+        tracing::error!(target: Log::GameReporter, ?e, "Write res error");
+        return 0;
+    }
+    let res = encoder.finish();
+
+    match res {
+        Ok(res) => {
+            tracing::warn!(target: Log::GameReporter, "Compression success");
+            res.len()
+        },
+        Err(e) => {
+            tracing::error!(target: Log::GameReporter, ?e, "Compression error");
+            0
+        },
+    }
+}
+
 /// Attempts to compress and upload replay data to the url at `upload_url`.
-fn try_upload_replay_data(data: Vec<u8>, upload_url: String) {
+fn try_upload_replay_data(data: Vec<u8>, upload_url: String, http_client: &ureq::Agent) {
+    tracing::warn!(target: Log::GameReporter, "Starting upload to: {}", upload_url);
+
     //X-Goog-Content-Length-Range: 0,10000000
+    let raw_data_size = data.len() as u32;
+    let rdbs = raw_data_size.to_le_bytes();
+
+    tracing::warn!(
+        target: Log::GameReporter,
+        "About to combine {} {} {} {}",
+        rdbs[3],
+        rdbs[2],
+        rdbs[1],
+        rdbs[0]
+    );
+
+    // Add header and footer to replay file
+    let mut contents = vec![
+        b'{', b'U', 3, b'r', b'a', b'w', b'[', b'$', b'U', b'#', b'l', rdbs[3], rdbs[2], rdbs[1], rdbs[0],
+    ];
+    contents.extend_from_slice(&data);
+    let mut footer = vec![b'U', 8, b'm', b'e', b't', b'a', b'd', b'a', b't', b'a', b'{', b'}', b'}'];
+    contents.append(&mut footer);
+
+    tracing::warn!(target: Log::GameReporter, "About to compress");
+
+    let mut gzipped_data = vec![0u8; data.len()]; // Resize to some initial size
+    let res_size = compress_to_gzip(&contents, &mut gzipped_data);
+    if res_size == 0 {
+        return;
+    }
+    gzipped_data.resize(res_size, 0);
+
+    tracing::warn!(
+        target: Log::GameReporter,
+        "About to send bytes. Compression {} => {}",
+        data.len(),
+        gzipped_data.len(),
+    );
+
+    let response = http_client.put(upload_url.as_str()).send_bytes(&gzipped_data);
+
+    tracing::warn!(target: Log::GameReporter, resp = ?response);
+
+    match response {
+        Ok(response) => {
+            tracing::warn!(
+                target: Log::GameReporter,
+                "Replay upload success! Response: {}",
+                response.into_string().unwrap_or_default()
+            );
+        },
+        Err(e) => {
+            tracing::error!(
+                target: Log::GameReporter,
+                ?e,
+                "Failed to upload replay data to {}",
+                upload_url,
+            );
+        },
+    }
 }
