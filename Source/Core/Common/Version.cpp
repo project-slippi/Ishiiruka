95528860bc88354af5d1c0742ee7f180402c410c
Jas Laferriere <Fizzi36@gmail.com>
2026-04-28 13:44:16 -0400

    chore: reset version

--- Source/Core/Common/Version.cpp
+++ Source/Core/Common/Version.cpp
@@ -27,7 +27,7 @@
 // if releasing both builds, use `release: {combined_version}` or `release: {netplay_version} | {playback_version}`
 // for releasing a single build, use `release(netplay): {netplay_version}` or `release(playback): {playback_version}`
 #ifndef IS_PLAYBACK
-#define SLIPPI_REV_STR "3.5.3-dev.1" // netplay version
+#define SLIPPI_REV_STR "3.5.2" // netplay version
 #else
 #define SLIPPI_REV_STR "3.5.2" // playback version
 #endif
