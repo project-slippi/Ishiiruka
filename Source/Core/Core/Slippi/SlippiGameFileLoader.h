68e557f6903298057677132a9d415ad91d787813
Jas Laferriere <Fizzi36@gmail.com>
2026-09-25 14:31:56 -0400

    add slippi file content links

--- Source/Core/Core/Slippi/SlippiGameFileLoader.h
+++ Source/Core/Core/Slippi/SlippiGameFileLoader.h
@@ -16,6 +16,14 @@ class SlippiGameFileLoader
 	std::set<std::string> grpsx_strings = {"GrPs1.dat", "GrPs2.dat", "GrPs3.dat", "GrPs4.dat"};
 
   protected:
+	// Replaces the slp: links in an archive with the assets they name, read from the disc. Returns
+	// false and empties contents when a link could not be resolved
+	bool resolveLinks(const std::string &fileName, std::string &contents);
+	static bool readDiscFile(void *ctx, const char *fileName, const u8 **outData, uintptr_t *outLen);
+
 	std::unordered_map<std::string, std::string> fileCache;
 	open_vcdiff::VCDiffDecoder decoder;
+
+	// Holds the disc file most recently handed to Rust by readDiscFile
+	std::vector<u8> discReadBuffer;
 };
